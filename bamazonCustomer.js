521c18ca498c302396f532bfa71a60d696bd9935
josef utleg <josefutleg@gmail.com>
2018-08-14 18:08:25 -0700

    completed node for customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var cmd;
+var inputID;
+var inputQuant;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,63 +13,102 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-var cmd;
-inquirer
+function startUp() {
+  inquirer
     .prompt([
-        {
-            type: 'list',
-            message: 'Welcome! What would you like to drink?',
-            choices: ['Beer', 'Whiskey', 'Cognac', 'Misc'],
-            name: 'command'
-        }
+      {
+        type: "list",
+        message: "Welcome! What would you like to drink?",
+        choices: ["Beer", "Whiskey", "Cognac", "Misc"],
+        name: "command"
+      }
     ])
-    .then(function(inquirerResponse){
-        cmd = inquirerResponse.command;
-        drinkList();
-    })
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-//   listAll();
-    // testAll();
-  
-});
-
-function listAll(){
-    connection.query('SELECT * FROM products', function(err, res){
-        for (var i in res){
-            console.log(res[i].department_name);
-        }
-        connection.end();
-    })
-    
+    .then(function(inquirerResponse) {
+      cmd = inquirerResponse.command;
+      drinkList();
+    });
 }
 
-function testAll(){
-    connection.query('SELECT * FROM products', function(err, res){
-        console.log(res);
-        connection.end();
-    })
-    
+startUp();
+
+function drinkList() {
+  connection.query(
+    "SELECT * FROM products WHERE department_name=?",
+    [`${cmd}`],
+    function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(
+          `Item ID:${res[i].item_id} |`,
+          `Product:${res[i].product_name} |`,
+          `Price:$${res[i].price}`
+        );
+      }
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "Input an item's ID that you would like",
+            name: "item_id"
+          },
+          {
+            type: "input",
+            message: "How many would you like?",
+            name: "stock"
+          }
+        ])
+        .then(function(data) {
+          inputID = parseFloat(data.item_id);
+          inputQuant = parseFloat(data.stock);
+          checkStock();
+        });
+    }
+  );
 }
 
-function drinkList(){
-    connection.query('SELECT * FROM products WHERE department_name=?', [`${cmd}`], function(err, res){
-        inquirer
+function checkStock() {
+  connection.query(
+    "SELECT * FROM products WHERE item_id=?",
+    [`${inputID}`],
+    function(err, res) {
+      for (var p in res) {
+        var prod = res[p].product_name;
+        var stock = parseFloat(res[p].stock_quantity);
+        var newStock = parseFloat(stock - inputQuant);
+        var tax = parseFloat(res[p].price * inputQuant * 0.0725);
+        var finalPrice = parseFloat(res[p].price * inputQuant + tax).toFixed(2);
+
+        if (inputQuant > stock) {
+          console.log("oops! we do not have enough!");
+          connection.end();
+          return;
+        } else {
+          inquirer
             .prompt([
-                {
-                    type: 'list',
-                    message: `What Type of ${cmd} Would You Like?`,
-                    choices: [res[i].product_name],
-                    name: 'command'
-                }
+              {
+                type: "list",
+                message: `${inputQuant} of ${prod} for $${finalPrice}. Checkout?`,
+                choices: ["Yes!", "I Changed My Mind!"],
+                name: "command"
+              }
             ])
-        for(var i = 0; i < res.length; i++){        
-            
-            console.log(res[i].product_name);
+            .then(function(checkout) {
+              var op = checkout.command;
+              if (op == "Yes!") {
+                var query = connection.query(
+                  `UPDATE products
+                  SET stock_quantity=${newStock}
+                  WHERE item_id=${inputID}`,
+                  function(err, data) {
+                    console.log("Done! Enjoy!");
+                    connection.end();
+                  }
+                );
+              } else {
+                startUp();
+              }
+            });
         }
-        // connection.end();
-    })
+      }
+    }
+  );
 }
-
